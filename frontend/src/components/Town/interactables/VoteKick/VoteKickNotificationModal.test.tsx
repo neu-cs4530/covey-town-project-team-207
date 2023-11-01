ffc11684eb3c65c83fa15b6431344dd8d4979492
Andrew <tsai.an@northeastern.edu>
2023-11-01 17:08:42 -0400

    fixed eslint errors

--- frontend/src/components/Town/interactables/VoteKick/VoteKickNotificationModal.test.tsx
+++ frontend/src/components/Town/interactables/VoteKick/VoteKickNotificationModal.test.tsx
@@ -1,37 +1,37 @@
 import { render, screen, act } from '@testing-library/react';
 import React from 'react';
 import NewVoteKickNotificationModal from './VoteKickNotificationModal';
-  
+
 describe('NewVoteKickNotificationModal', () => {
-    jest.setTimeout(15000);
-    it('should render with the correct initial values', () => {
-        render(<NewVoteKickNotificationModal username='Andrew'/>);
-        expect(screen.getByText(/Disruptive user identified, votekick for/)).toBeInTheDocument();
-        expect(screen.getByText(/Andrew/)).toBeInTheDocument();
-        expect(screen.getByText(/occuring in:/)).toBeInTheDocument();
-        expect(screen.getByText(/10 seconds/)).toBeInTheDocument();
+  jest.setTimeout(15000);
+  it('should render with the correct initial values', () => {
+    render(<NewVoteKickNotificationModal username='Andrew' />);
+    expect(screen.getByText(/Disruptive user identified, votekick for/)).toBeInTheDocument();
+    expect(screen.getByText(/Andrew/)).toBeInTheDocument();
+    expect(screen.getByText(/occuring in:/)).toBeInTheDocument();
+    expect(screen.getByText(/10 seconds/)).toBeInTheDocument();
+  });
+  it('should count down the timer correctly', async () => {
+    render(<NewVoteKickNotificationModal username='Andrew' />);
+    expect(screen.getByText(/10 seconds/)).toBeInTheDocument();
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 1100));
     });
-    it('should count down the timer correctly', async () => {
-        render(<NewVoteKickNotificationModal username='Andrew'/>);
-        expect(screen.getByText(/10 seconds/)).toBeInTheDocument();
-        await act(async () => {
-            await new Promise((resolve) => setTimeout(resolve, 1100));
-        });
-        expect(screen.getByText(/9 seconds/)).toBeInTheDocument();
-        await act(async () => {
-            await new Promise((resolve) => setTimeout(resolve, 6100));
-        });
-        expect(screen.getByText(/3 seconds/)).toBeInTheDocument();
+    expect(screen.getByText(/9 seconds/)).toBeInTheDocument();
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 6100));
     });
-    it('should close when timer hits 0', async () => {
-        render(<NewVoteKickNotificationModal username='Andrew'/>);
-        expect(screen.getByText(/10 seconds/)).toBeInTheDocument();
-        await act(async () => {
-            await new Promise((resolve) => setTimeout(resolve, 10500));
-        });
-        expect(screen.queryByText(/Disruptive user identified, votekick for/)).toBeNull();
-        expect(screen.queryByText(/Andrew/)).toBeNull();
-        expect(screen.queryByText(/occuring in:/)).toBeNull();
-        expect(screen.queryByText(/0 seconds/)).toBeNull();
+    expect(screen.getByText(/3 seconds/)).toBeInTheDocument();
+  });
+  it('should close when timer hits 0', async () => {
+    render(<NewVoteKickNotificationModal username='Andrew' />);
+    expect(screen.getByText(/10 seconds/)).toBeInTheDocument();
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 10500));
     });
+    expect(screen.queryByText(/Disruptive user identified, votekick for/)).toBeNull();
+    expect(screen.queryByText(/Andrew/)).toBeNull();
+    expect(screen.queryByText(/occuring in:/)).toBeNull();
+    expect(screen.queryByText(/0 seconds/)).toBeNull();
+  });
 });
--- frontend/src/components/Town/interactables/VoteKick/VoteKickNotificationModal.tsx
+++ frontend/src/components/Town/interactables/VoteKick/VoteKickNotificationModal.tsx
@@ -1,40 +1,39 @@
 import {
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalHeader,
-    ModalOverlay,
-  } from '@chakra-ui/react';
-  import React, { useEffect, useState } from 'react';
-  
-  export default function NewVoteKickNotificationModal({username}: { username: string; }): JSX.Element {
-    const [timer, setTimer] = useState<number>(10);
-  
-    useEffect(() => {
-      if (timer > 0) {
-        const countdown = setInterval(() => {
-          setTimer(timer - 1);
-        }, 1000);
-        return () => {
-          clearInterval(countdown);
-        };
-      }
-    }, [timer]);
-  
-    return (
-      <Modal
-        isOpen={username !== undefined && timer > 0}
-        onClose={() => {}}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Disruptive user identified, votekick for {username} occuring in:</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            {timer} seconds
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    );
-  }
-  
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+
+export default function NewVoteKickNotificationModal({
+  username,
+}: {
+  username: string;
+}): JSX.Element {
+  const [timer, setTimer] = useState<number>(10);
+
+  useEffect(() => {
+    if (timer > 0) {
+      const countdown = setInterval(() => {
+        setTimer(timer - 1);
+      }, 1000);
+      return () => {
+        clearInterval(countdown);
+      };
+    }
+  }, [timer]);
+
+  return (
+    <Modal isOpen={username !== undefined && timer > 0} onClose={() => {}}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Disruptive user identified, votekick for {username} occuring in:</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>{timer} seconds</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
