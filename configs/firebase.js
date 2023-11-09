ebb60ec0386ab3e02b8d2ca710a1e6625a45d998
jaewock <jaewooksemail@gmail.com>
2023-11-09 13:28:52 -0500

    implemented firebase and google login

---
+++ configs/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmWNt6lQLN61lwKGJCtR5x8mIHJz8CVoU",
+    authDomain: "covey-town-207.firebaseapp.com",
+    projectId: "covey-town-207",
+    storageBucket: "covey-town-207.appspot.com",
+    messagingSenderId: "81906889675",
+    appId: "1:81906889675:web:1e019a298518a00f777c4e",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
