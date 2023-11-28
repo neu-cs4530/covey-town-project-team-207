9addb6dec789ace9881a29a39de628c2f52c4202
Cubostar <cubostar@gmail.com>
2023-11-27 20:47:00 -0500

    Make votekick modal

--- frontend/src/components/Town/interactables/VoteKick/VoteKickNotificationModal.tsx
+++ frontend/src/components/Town/interactables/VoteKick/VoteKickNotificationModal.tsx
@@ -5,34 +5,67 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  useDisclosure,
+  Text,
+  ModalFooter,
+  Button,
+  useToast,
 } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
+import VoteKickNotificationModalController from '../../../../classes/interactable/VoteKickNotificationModalController';
+import useTownController from '../../../../hooks/useTownController';
 
-export default function NewVoteKickNotificationModal({
+export default function VoteKickNotificationModal({
   username,
+  controller,
 }: {
   username: string;
+  controller: VoteKickNotificationModalController 
 }): JSX.Element {
-  const [timer, setTimer] = useState<number>(10);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const townController = useTownController();
+  const [playerToKick, setPlayerToKick] = useState<string>('');
 
   useEffect(() => {
-    if (timer > 0) {
-      const countdown = setInterval(() => {
-        setTimer(timer - 1);
-      }, 1000);
-      return () => {
-        clearInterval(countdown);
-      };
-    }
-  }, [timer]);
+    const handleVoteKick = (offendingPlayer: PlayerController) => {
+      onOpen();
+      setPlayerToKick(offendingPlayer.userName);
+    };
+    townController.addListener('playerVoteKick', handleVoteKick);
+    return () => {
+      townController.removeListener('playerVoteKick', handleVoteKick);
+    };
+  }, [townController]);
 
   return (
-    <Modal isOpen={username !== undefined && timer > 0} onClose={() => {}}>
+    <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Disruptive user identified, votekick for {username} occuring in:</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody pb={6}>{timer} seconds</ModalBody>
+        <ModalHeader>Votekick for {username}</ModalHeader>
+        <ModalBody>
+          <Text>
+            User {username} has said inappropriate language multiple times. Please vote to kick or not kick them from the town.
+          </Text>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            colorScheme='green'
+            onClick={() => {
+              controller.recordVote(true);
+              onClose();
+            }}>
+            Don't Kick
+          </Button>
+          <Button
+            colorScheme='red'
+            onClick={() => {
+              controller.recordVote(false);
+              onClose();
+            }}>
+            Kick
+          </Button>
+        </ModalFooter>
       </ModalContent>
     </Modal>
   );
