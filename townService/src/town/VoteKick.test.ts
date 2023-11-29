70854355a778210ddd0a9a312f8d9837b5b44f1c
Cubostar <cubostar@gmail.com>
2023-11-29 00:36:30 -0500

    Finish VoteKick tests

--- townService/src/town/VoteKick.test.ts
+++ townService/src/town/VoteKick.test.ts
@@ -15,51 +15,72 @@ describe('Tests for VoteKick Model', () => {
     model = new VoteKick(playerToKick);
     player = new Player(nanoid(), townEmitter);
   });
-  it('Should create a VoteKick object with the correct username and an empty vote dictionary', () => {
+  it('Should create a VoteKick object with the correct id and an empty vote dictionary', () => {
     expect(model).toBeDefined();
     expect(model.votes).toEqual({});
     expect(model.playerToKick).toEqual(playerToKick);
   });
   it('Should add a positive vote to the vote dictionary when addVote is called with "true"', () => {
-    model.addVote(player.userName, true);
-    expect(model.votes).toEqual({ [player.userName]: true });
+    model.addVote(player.id, true);
+    expect(model.votes).toEqual({ [player.id]: true });
   });
   it('Should add a negative vote to the vote dictionary when addVote is called with "false"', () => {
-    model.addVote(player.userName, false);
-    expect(model.votes).toEqual({ [player.userName]: false });
+    model.addVote(player.id, false);
+    expect(model.votes).toEqual({ [player.id]: false });
   });
   it('Should return true when hasVoted is called with a player who has voted', () => {
-    model.addVote(player.userName, true);
-    expect(model.hasVoted(player.userName)).toEqual(true);
+    model.addVote(player.id, true);
+    expect(model.hasVoted(player.id)).toEqual(true);
   });
   it('Should return false when hasVoted is called with a player who has not voted', () => {
-    expect(model.hasVoted(player.userName)).toEqual(false);
+    expect(model.hasVoted(player.id)).toEqual(false);
   });
   it('Should throw an error when addVote is called with a player who has already voted', () => {
-    model.addVote(player.userName, true);
-    expect(model.hasVoted(player.userName)).toEqual(true);
-    expect(() => model.addVote(player.userName, true)).toThrowError();
+    model.addVote(player.id, true);
+    expect(model.hasVoted(player.id)).toEqual(true);
+    expect(() => model.addVote(player.id, true)).toThrowError();
   });
   it('Should handle multiple votes correctly', () => {
     const player2 = new Player(nanoid(), townEmitter);
-    model.addVote(player.userName, true);
-    model.addVote(player2.userName, false);
-    expect(model.votes).toEqual({ [player.userName]: true, [player2.userName]: false });
+    model.addVote(player.id, true);
+    model.addVote(player2.id, false);
+    expect(model.votes).toEqual({ [player.id]: true, [player2.id]: false });
   });
   it('Should return true when voteKickSuccessful is called with a majority of positive votes', () => {
     const player2 = new Player(nanoid(), townEmitter);
     const player3 = new Player(nanoid(), townEmitter);
-    model.addVote(player.userName, true);
-    model.addVote(player2.userName, true);
-    model.addVote(player3.userName, false);
+    model.addVote(player.id, true);
+    model.addVote(player2.id, true);
+    model.addVote(player3.id, false);
     expect(model.voteKickSuccessful()).toEqual(true);
   });
   it('Should return false when voteKickSuccessful is called with a minority of positive votes', () => {
     const player2 = new Player(nanoid(), townEmitter);
     const player3 = new Player(nanoid(), townEmitter);
-    model.addVote(player.userName, true);
-    model.addVote(player2.userName, false);
-    model.addVote(player3.userName, false);
+    model.addVote(player.id, true);
+    model.addVote(player2.id, false);
+    model.addVote(player3.id, false);
     expect(model.voteKickSuccessful()).toEqual(false);
+  });
+  it('Should return true when isVotingDone is called with a majority of players voting in favor', () => {
+    const player2 = new Player(nanoid(), townEmitter);
+    const player3 = new Player(nanoid(), townEmitter);
+    model.addVote(player2.id, true);
+    model.addVote(player3.id, true);
+    expect(model.isVotingDone([player, player2, player3])).toEqual(true);
+  });
+  it('Should return true when isVotingDone is called with a majority of players voting in disfavor', () => {
+    const player2 = new Player(nanoid(), townEmitter);
+    const player3 = new Player(nanoid(), townEmitter);
+    model.addVote(player2.id, false);
+    model.addVote(player3.id, false);
+    expect(model.isVotingDone([player, player2, player3])).toEqual(true);
+  });
+  it('Should return false when isVotingDone is called with a majority not voting for the same outcome', () => {
+    const player2 = new Player(nanoid(), townEmitter);
+    const player3 = new Player(nanoid(), townEmitter);
+    model.addVote(player.id, true);
+    model.addVote(player3.id, false);
+    expect(model.isVotingDone([player, player2, player3])).toEqual(false);
   });
 });
